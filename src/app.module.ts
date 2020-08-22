f15ce789e0449397f2453740238b333cfbfb4305
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-08-22 12:46:46 +0530

    Security Service Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { ModelService } from './model/model.service';
+import { SecurityService } from './security/security.service';
 
 
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, DatabaseService, ModelService],
+  providers: [AppService, DatabaseService, ModelService, SecurityService],
 })
 export class AppModule {}
