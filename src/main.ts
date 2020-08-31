a8f660f550253f8cbedb3159bef96bd3eafdcdae
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-08-31 23:53:34 +0530

    Updated To EJs

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname,'..','public'))
   app.setBaseViewsDir(join(__dirname,'..','views'))
-  app.setViewEngine('pug')
+  app.setViewEngine('ejs')
   await app.listen(3000)
-
-
 }
 
 bootstrap();
