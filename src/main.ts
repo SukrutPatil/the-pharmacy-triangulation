27bee93424670933bc43be9d8300a97f577e2747
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-09-03 20:35:42 +0530

    Database Backup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {NestExpressApplication} from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import {NestExpressApplication} from '@nestjs/platform-express'
 import {join} from 'path'
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,5 +9,4 @@ async function bootstrap() {
   app.setViewEngine('ejs')
   await app.listen(3000)
 }
-
 bootstrap();
