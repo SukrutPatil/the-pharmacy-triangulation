f7ddf25935b00c32f3e4d5dfe4702ccbf46d9a92
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-09-14 09:39:54 +0530

    Express Session Added

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.enableCors();
+  app.use(session({
+    secret: 'MadeByJaskiratSukrutSumit',
+    resave: true,
+    saveUninitialized:false,
+  }));
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
