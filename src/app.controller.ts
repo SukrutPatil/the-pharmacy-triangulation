c37bd02689626317e266a350580ff46da5da22ce
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-09-21 22:40:53 +0530

    Ab chalega bro

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {Request} from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @Render('Homepage.ejs')
-  getHomePage(): any {
-    return { loggedInUser: '' };
+  getHomePage(@Req() req:Request): any {
+    return { loggedInUser: req.session.loggedInUser };
   }
 
   @Get('aboutUs')
