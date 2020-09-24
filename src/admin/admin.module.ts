a7f2747345d147bc2345737ddd3da65fa0c3d602
JSGREWAL-FX-NOT\JSGREWAL <jsgrewal1204@gmail.com>
2020-09-24 11:11:57 +0530

    ModuleErrorResolved

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { DatabaseService } from '../database/database.service';
 import { ModelService } from '../model/model.service';
+import { SecurityService } from '../security/security.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [SessionExecutorService,DatabaseService,ModelService],
+  providers: [SessionExecutorService,DatabaseService,ModelService,SecurityService],
 })
 export class AdminModule {}
