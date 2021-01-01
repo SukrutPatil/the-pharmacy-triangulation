1eaca42e42a4e0bd2d2a4af2c90c4360941d26fa
Jaskirat <thejscode@pop-os.localdomain>
2021-01-01 21:55:58 +0530

    gvifhdgvoefhgrl;g

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,16 @@
 import { Controller, Get, Render, Req, Res } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Request, Response } from 'express';
 import { DatabaseService, EntryType } from './database/database.service';
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly db: DatabaseService,
-  ) {}
+  constructor(private readonly db: DatabaseService) {}
 
   @Get()
   @Render('Homepage.ejs')
   async getHomePage(@Req() req: Request, @Res() res: Response): Promise<any> {
     //Fetch products Data
     const theDBReturnObject = await this.db.retrieve(EntryType.DRUG);
+    const prod_id: Array<string> =[];
     const prod_name: Array<string> = [];
     const prod_price: Array<string> = [];
     const prod_img: Array<string> = [];
@@ -22,12 +19,19 @@ export class AppController {
       res.status(501);
     }
     theDBReturnObject.resultObject.rows.forEach(productEntry => {
-      const { id, name,  mrp, imgaddress } = productEntry;
+      const { id, name, mrp, imgaddress } = productEntry;
+      prod_id.push(id);
       prod_name.push(name);
       prod_price.push(mrp);
       prod_img.push(imgaddress);
     });
-    return { loggedInUser: req.session.loggedInUser, prod_name: prod_name,prod_price:prod_price,prod_img:prod_img };
+    return {
+      loggedInUser: req.session.loggedInUser,
+      prod_id,
+      prod_name: prod_name,
+      prod_price: prod_price,
+      prod_img: prod_img,
+    };
   }
 
   @Get('aboutUs')
